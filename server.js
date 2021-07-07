a38bb11c974368860b8427d15294f019bd3763ff
EmerickSAMSON <emerick77@gmail.com>
2021-07-07 12:26:50 +0200

    Ajout routes /heroes/id, /heroes/bio/id, DELETE /heroes/id

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const { ObjectId } = require('mongodb')
 
 // Connection à la BDD
 
 fastify.register(require('fastify-mongodb'), {
   // force to close the mongodb connection when app stopped
   // the default value is false
-  forceClose: true,
-  
-  url: 'mongodb://localhost:27017/superheroes'
+    forceClose: true,
+    
+    url: 'mongodb://localhost:27017/superheroes'
 })
 
 //METHOD API REST
@@ -28,16 +29,35 @@ const avengers = ["Iron Man", "Captain America", "Spiderman"]
 
 // /heroes GET - obtiens la liste des héros
 
-fastify.get('/heroes', () => {
-    return avengers
+fastify.get('/heroes', async() => {
+    const collection = fastify.mongo.db.collection("heroes")
+    const result = await collection.find({}).toArray()
+    return result
+})
+
+// /heroes/69 GET -obtiens le héros avec l'ID 69
+// /heroes/heroesId ... findOne()
+
+fastify.get('/heroes/:heroesId', async(request, reply) => {
+
+    // Documentation : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    // const heroesId = request.params.heroesId
+    const { heroesId } = request.params
+    const collection = fastify.mongo.db.collection("heroes")
+    const result = await collection.findOne({
+        _id: new ObjectId(heroesId)
+    })
+    return result 
 })
 
 // /heroes POST - creer un nouvel héros
 
-fastify.post('/heroes', (request, reply) => {
+fastify.post('/heroes', async(request, reply) => {
     const collection = fastify.mongo.db.collection("heroes")
-    collection.insertOne(request.body)
-    return null
+    const result = await collection.insertOne(request.body)
+    return result.ops[0].name
+    //.ops permet de renvoyer un tableau sans les infos du serveur
+    // le .name permet d'avoir uniquement le nom de l'indice 0 du tableau
 })
 
 fastify.get('/me', function(){
@@ -46,6 +66,32 @@ fastify.get('/me', function(){
         nom: "SAMSON",
         job: "ingé son",
     }
+})
+
+// /heroes/bio/id
+// Cette route devra retourné: nomDuHero connu sous le nom de vraiNom. Je suis née à lieuDeNaissance. J'ai XX en intelligence, et YY en vitesse.
+
+
+fastify.get('/heroes/bio/:heroesId', async (request, reply) => {
+	const collection = fastify.mongo.db.collection('heroes')
+	const { heroesId } = request.params
+	const result = await collection.findOne({
+		_id: new ObjectId(heroesId)
+	})
+
+    const { name, biography, powerstats: {intelligence, speed} } = result
+    // const { speed, intelligence } = powerstats
+	// Template literals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+	return `${name} connu sous le nom de ${biography["full-name"]}. Je suis née à ${biography["place-of-birth"]}. J'ai ${intelligence} en intelligence, et ${speed} en vitesse.`
+})
+
+fastify.delete('/heroes/:heroesId', async (request, reply) => {
+    const collection = fastify.mongo.db.collection('heroes')
+    const {heroesId} = request.params
+    const result = await collection.findOneAndDelete({
+        _id: new ObjectId(heroesId)
+    })
+    return result
 })
 
 // Run the server!
